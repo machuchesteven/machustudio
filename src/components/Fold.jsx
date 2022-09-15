367bf6cb0c82f40fa9babd7eb9c96b898e268ddd
Machuche Steven <machuchesteven@gmail.com>
2022-09-15 13:46:55 +0300

    Redesign of the Real Page

--- src/components/Fold.jsx
+++ src/components/Fold.jsx
@@ -1,41 +1,35 @@
 import React from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Heading, Text, ChakraProvider, keyframes } from '@chakra-ui/react'
+
+
+import { animations } from 'framer-motion';
+import '../styles/Fold.css'
 
 
 const Fold = () => {
     return (
-        <Container>
-            <Row>
-                <Col sm={12} md={6}>
-                    <h1>
-                        <span>MachuStudio:</span> The Epicenter Of Designing
-                    </h1>
-                    <p className="text-lg">We design and engineer digital arts from imagination</p>
-                    <p>Starting with <span>2D Graphics, </span>
-                        <span>3D Models,</span> <span>UI / UX Designs</span> and <span>Systems Designing</span>
-                    </p>
-                    <hr />
-                    <Row g={2}>
-                        <Col md={6}><Button variant="outline-dark" className="w-100 mt-2 shadow-sm">Hire Us</Button></Col>
-                        <Col md={6}><Button variant="dark" className="w-100 mt-2 shadow-sm">See More Works</Button></Col>
-                    </Row>
-                    <Row className="mt-2">
-                        <Col md={6} sm={5} className="justify-content-center">
-                            <span>Rating Stars</span>
-                            <hr />
-                            <p>On Behance</p>
-                        </Col>
-                        <Col md={6} sm={7} className="justify-content-center">
-                            <span>Counter +</span><hr />
-                            <p>Completed Projects</p>
-                        </Col>
-                    </Row>
-
-                </Col>
-                <Col sm={12} md={6}>
-                    <h1>The illustration goes here</h1>
-                </Col>
-            </Row>
+        <Container fluid style={{ justifyContent: 'center' }} className="mt-md-5">
+            <Container>
+                <ChakraProvider>
+                    <Heading className='text-center'>MachuStudio: The epicenter of <Text as={'span'} className='line-1 typewriter_text' bgGradient='linear(to-l, #79a8CA, #ff5580)' bgClip='text'>Creativity</Text></Heading>
+                    <Text fontSize='lg' className='text-center m-md-5'> We Design and <Text as={'span'}
+                        bgGradient='linear(to-l, #7928CA, #FF0080)'
+                        bgClip='text'
+                        fontSize='lg'
+                        fontWeight='medium'
+                    >
+                        We Design and Engineer Digital Arts from imagination
+                    </Text>
+                    </Text>
+                    <Container className="justufy-content-evenly px-5">
+                    <Button variant="outline-dark" className='mx-5 px-5 my-2 py-2'>
+                        See More Designs
+                    </Button>
+                    <Button variant="dark" className='mx-5 px-5 my-2 py-2'>Hire Us</Button>
+                    </Container>
+                </ChakraProvider>
+            </Container>
         </Container>
     )
 }
